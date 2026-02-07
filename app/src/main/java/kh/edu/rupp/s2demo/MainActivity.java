c8336e6de49e1df4658ae01ee02efcb45f22bf63
kimsong <saokimsong@gmail.com>
2026-02-07 09:52:19 +0700

    [Task 1]: Update Main Activity Class

--- app/src/main/java/kh/edu/rupp/s2demo/MainActivity.java
+++ app/src/main/java/kh/edu/rupp/s2demo/MainActivity.java
@@ -1,4 +1,13 @@
 package kh.edu.rupp.s2demo;
 
-public class MainActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
